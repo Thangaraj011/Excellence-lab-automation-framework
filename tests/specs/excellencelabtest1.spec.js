1df3774cf347b3936c93ec92032888d62a2298ce
Thangaraj_R <thangaraj.r@searce.com>
2026-06-03 18:52:34 +0530

    small change added

--- tests/specs/excellencelabtest1.spec.js
+++ tests/specs/excellencelabtest1.spec.js
@@ -6,15 +6,14 @@ test.describe('Authentication Tests', () => {
   test('Employee Homepage validation', {tag: '@smoke'}, async ({},testInfo) => {
     const contextSetup = new TestContextSetup(testInfo);
     const page = await contextSetup.init(`${process.env.BASE_BE_URL}`, 'chromium');
+    const currentMap = await contextSetup.genericUtils.jsonToMap();
+    console.log(await currentMap.get('IP_Title'));
     const token = await contextSetup.genericUtils.generateJWT(process.env.EMP_USERNAME);
     const page2 = await contextSetup.openNewTab();
     await page2.goto(`${process.env.BASE_FE_URL}`);
     await contextSetup.genericUtils.setupAuthCookie(token);
     await page2.reload();
     await page2.waitForLoadState('networkidle');
-    await contextSetup.genericUtils.setJsonData();
-    console.log(await contextSetup.genericUtils.getValueByKey('IP_Title'));
-
 
   });
 
